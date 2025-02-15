5f03f6d9f6df81b504cce2c8ed598bd7149fc66a
jayastronomic <juliansmith94@hotmail.com>
2025-02-15 00:49:27 -0600

    update FileData dto

--- src/main/java/com/example/filekeep/dtos/FileData.java
+++ src/main/java/com/example/filekeep/dtos/FileData.java
@@ -1,17 +1,21 @@
 package com.example.filekeep.dtos;
 
+import java.util.UUID;
+
 import com.example.filekeep.models.File;
 
 import lombok.Getter;
 
 @Getter
 public class FileData {
+    private UUID id;
     private String fileName;
     private long size;
     private String mimeType;
     private String fileKey;
 
     public FileData(File file) {
+        this.id = file.getId();
         this.fileName = file.getFileName();
         this.size = file.getSize();
         this.mimeType = file.getMimeType();
